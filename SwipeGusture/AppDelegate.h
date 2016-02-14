df6d1192eb0d0d24e18fa8a3bbf95fa30806a3c0
Sanjay Jha <sanjay@letsventure.com>
2016-02-14 14:46:18 +0530

    Initial Commit

---
+++ SwipeGusture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwipeGusture
+//
+//  Created by sanjay kumar jha on 14/02/16.
+//  Copyright © 2016 sanjay kumar jha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwipeGusture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwipeGusture
+//
+//  Created by sanjay kumar jha on 14/02/16.
+//  Copyright © 2016 sanjay kumar jha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
